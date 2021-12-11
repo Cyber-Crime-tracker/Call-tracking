7a425c834f0d56d2bc8489ec14a73a45e2d4bf6b
Cyber-Crime-tracker <76731045+Cyber-Crime-tracker@users.noreply.github.com>
2021-12-11 22:06:31 +0530

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,77 @@
+function ss(){
+			var a=document.getElementById("demo").value;
+			var b=document.getElementById("demo2").value;
+			
+			
+			if(a=="Aa"){
+				document.getElementById("demo1").innerHTML="<audio id=jana controls controlsList=nodownload><source src=call/sandeep.mp3 type=audio/mp3></audio><div style=text-align:left id=man><p>Call tracked by <p id=rrr style=font-weight:bold>K.AFREEN (Network Hacker)</p></p><br><p id=rrr>Tracked Date: 14/07/2019</p><br><p id=rrr>Type: Outgoing</p><br><p id=rrr>Duration: 00:13:32</p></div>";
+				
+				
+			
+			}
+			
+			else if(a=="F7C7F591" && b=="192.168.27.70"){
+				
+				document.getElementById("demo1").innerHTML="<audio controls controlsList=nodownload><source src=call/m3.mp3 type=audio/mp3></audio><div style=text-align:left;padding-left:200px><p>Call tracked by <p style=font-weight:bold>K.AFREEN (Network Hacker)</p></p><br><p>Tracked Date: 12/02/2021</p><br><p>Type: Outgoing</p><br><p>Duration: 00:15:41</p></div>";
+				
+				
+			}
+			
+			else if(a=="A"){
+			
+				document.getElementById("demo1").innerHTML="<audio controls id=jana><source src=call/priya1.mp3 type=audio/mp3></audio>";
+			
+			}
+			
+			else if(a=="2156ACEA"){
+			
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya2.mp3 type=audio/mp3></audio>";
+			
+			
+			}
+			
+			else if(a=="5D8FFCC8"){
+			
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya3.mp3 type=audio/mp3></audio>";
+			}
+			
+			else if(a=="D21971F7"){
+			
+				document.getElementById("demo1").innerHTML="<audio controls controlsList=nodownload><source src=call/priya4.mp3 type=audio/mp3></audio>";
+			
+			}
+			
+			else if(a=="O2A6CC3A"){
+			
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya5.mp3 type=audio/mp3></audio>";
+			}
+				
+			else if(a=="3DBF1472"){
+			
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya6.mp3 type=audio/mp3></audio>";
+			}
+				
+			else if(a=="CE667DA1" && b=="192.168.56.97"){
+			
+				document.getElementById("demo1").innerHTML="<audio controls controlsList=nodownload><source src=call/m2.mp3 type=audio/mp3></audio><div style=text-align:left;padding-left:200px><p>Call tracked by <p style=font-weight:bold>K.AFREEN (Network Hacker)</p></p><br><p>Tracked Date: 12/08/2020</p><br><p>Type: Outgoing</p><br><p>Duration: 00:12:07</p></div>";
+			}
+	
+				
+			else if(a=="WDX34C66" && b=="192.168.72.88"){
+			
+				document.getElementById("demo1").innerHTML="<audio controls controlsList=nodownload><source src=call/m1.mp3 type=audio/mp3></audio><div style=text-align:left;padding-left:200px><p>Call tracked by <p style=font-weight:bold>K.AFREEN (Network Hacker)</p></p><br><p>Tracked Date: 17/11/2020</p><br><p>Type: Recording</p><br><p>Duration: 00:41:59</p></div>";
+				
+			}
+			else if(a=="437BA35A" && b=="192.168.54.62"){
+			
+				document.getElementById("demo1").innerHTML="<audio controls controlsList=nodownload><source src=call/panu.mp3 type=audio/mp3></audio><div style=text-align:left;padding-left:200px><p>Call tracked by <p style=font-weight:bold>K.AFREEN (Network Hacker)</p></p><br><p>Tracked Date: 18/12/2019</p><br><p>Type: Incoming</p><br><p>Duration: 00:02:04</p></div>";
+			}
+			else if(a=="12E26803" && b=="192.168.23.97"){
+			
+				document.getElementById("demo1").innerHTML="<audio controls controlsList=nodownload><source src=call/panu2.mp3 type=audio/mp3></audio><div style=text-align:left;padding-left:200px><p>Call tracked by <p style=font-weight:bold>K.AFREEN (Network Hacker)</p></p><br><p>Tracked Date: 27/01/2020</p><br><p>Type: Outgoing</p><br><p>Duration: 00:03:39</p></div>";
+			}
+			else{
+			document.getElementById("demo1").innerHTML="<p style=color:red;>Enter Correct Recorder IP & ID to Track Audio</p>";
+			}
+			
+			}
